98711d2697ba54677ff6f552b422084fdb980842
Michael Bortlik <Michael.Bortlik@gmx.de>
2013-05-29 06:14:11 +0200

    Hinzufügen von Funktionen

--- TEST/src/Schreiber.java
+++ TEST/src/Schreiber.java
@@ -4,9 +4,21 @@ public class Schreiber {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	
+	
+	public void blub()
+	{
+		System.out.println("hoohoho");
+	}
+	
+	
+	
+	public void blub(String hrhrhr) {
 		// TODO Auto-generated method stub
-
+		
+		
+		System.out.println("hihhi");
+		System.out.println( hrhrhr);
 	}
 
 }
